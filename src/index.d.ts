8eb240b551384165cf5f06201b1b2ffe70c16cc1
FloofyPlasma <github@floofyplasma.com>
2025-01-21 23:07:53 -0500

    feat(typescript): implement typescript support

---
+++ src/index.d.ts
@@ -0,0 +1,23 @@
+export = Framework;
+export as namespace Framework;
+
+declare namespace Framework {
+    export type Provider<T> = T & {
+        Name?: string
+        Uses?: any[]
+        Order?: number
+        Init?: () => void
+        Start?: () => void
+    }
+
+    export type Lifecycle<T extends any[]> = {
+        Name: string
+        Fire: (self: Lifecycle<T>, ...args: T) => void;
+        Listeners: Array<(...args: T) => void>;
+    }
+
+    export function Start(): void;
+    export function Add(dirs: Array<Instance>, filter?: (module: ModuleScript) => boolean): void;
+    export function New<T>(members: Provider<T>): T;
+    export function Lifecycle<T extends any[]>(name: string, callback?: (self: Lifecycle<T>, args: T) => void): Lifecycle<T>;
+}
