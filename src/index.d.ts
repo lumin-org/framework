464d6731333f5124a80629f2b64412018b0b4231
James <119134081+2jammers@users.noreply.github.com>
2025-01-28 22:01:16 -0600

    revert: add back provider type

--- src/index.d.ts
+++ src/index.d.ts
@@ -2,6 +2,14 @@ export = Framework;
 export as namespace Framework;
 
 declare namespace Framework {
+    export type Provider<T> = T & {
+        Name?: string
+        Uses?: any[]
+        Order?: number
+        Init?: () => void
+        Start?: () => void
+    }
+
     export type Lifecycle<T extends any[]> = {
         Name: string
         Fire: (self: Lifecycle<T>, ...args: T) => void;
